34fd757d5eac3156f9486bbcd86a664faae45a9a
Aka.Kiril <vuongtuan1211@gmail.com>
2022-06-02 12:22:10 +0700

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+const { Client } = require("@notionhq/client");
+
+const notion:any = new Client({ auth: process.env.NOTION_API_KEY  });
+
 
 type Data = {
-  name: string
-}
+    name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: any, res: any) {
+    const response: any = await notion.databases.retrieve({
+        database_id: process.env.NOTION_DATABASE_ID,
+    });
+    res.status(200).json({response});
 }
