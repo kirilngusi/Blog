166691589cbfcc0c7e31f98c060736ada851193d
kirilngusi <vuongtuan1211@gmail.com>
2025-04-12 17:46:39 +0700

    chore: clear same UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,23 +18,51 @@ import "prismjs/themes/prism-tomorrow.css";
 import "katex/dist/katex.min.css";
 import Head from "next/head";
 import ReactGA from "react-ga4";
-ReactGA.initialize("G-KCMPTVY7L2");
+import { useEffect } from "react";
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-    <Head>
-        <meta charSet="UTF-8" />
-        <meta name="keywords" content="titla, meta, nextjs" />
-        <meta name="author" content="KirilDev" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </Head>;
+    useEffect(() => {
+        ReactGA.initialize("G-KCMPTVY7L2");
+
+        ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
+        const handleRouteChange = (url: string) => {
+            ReactGA.send({ hitType: "pageview", page: url });
+        };
+
+        Router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            Router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, []);
     return (
+        <>
+        <Head>
+            <meta charSet="UTF-8" />
+            <meta name="keywords" content="titla, meta, nextjs" />
+            <meta name="author" content="KirilDev" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" href="/favicon.ico" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#000000" />
+            <meta name="description" content="KirilNgusi's personal website" />
+            <meta name="keywords" content="KirilNgusi, Kiril, KirilNgusi's personal website" />
+            <meta name="author" content="KirilNgusi" />
+            <meta property="og:title" content="KirilNgusi" />
+            <meta property="og:description" content="KirilNgusi's personal website" />
+            <meta property="og:image" content="/favicon.ico" />
+            <meta property="og:url" content="https://kirilngusi.vercel.app/" />
+            <meta property="og:type" content="website" />
+        </Head>
         <Layout>
             <Component {...pageProps} />
         </Layout>
+        </>
     );
 }
 
